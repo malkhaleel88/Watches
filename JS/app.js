89098c9a54663d61fa18b4cc89155857abafa3d8
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-05-23 18:49:00 +0300

    create new page

---
+++ JS/app.js
@@ -0,0 +1,17 @@
+
+alert ('Welcome to Watches Gallery')
+
+let UserName = prompt('What is your name ?')
+console.log(UserName)
+
+let UserCountry = prompt('What is your country ?')
+console.log(UserCountry)
+
+let UserAge = prompt('What is your age ?')
+console.log(UserAge)
+
+let UserBrand = prompt('What is your favourite brand ?')
+console.log(UserBrand)
+
+
+alert (' Hello '+ UserName +' from '+ UserCountry +' with '+UserAge +' years old '+'find your ' +UserBrand+' latest models ')
