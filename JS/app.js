a28c4475edae6348aaefdf9ba7c95d14ead99860
malkhaleel88 <m.alkhaleel@yahoo.com>
2021-05-24 10:32:26 +0300

    modify

--- JS/app.js
+++ JS/app.js
@@ -2,16 +2,21 @@
 alert ('Welcome to Watches Gallery')
 
 let UserName = prompt('What is your name ?')
+document.getElementById("one").innerHTML = UserName;
 console.log(UserName)
 
 let UserCountry = prompt('What is your country ?')
+document.getElementById("two").innerHTML = UserCountry;
 console.log(UserCountry)
 
 let UserAge = prompt('What is your age ?')
+document.getElementById("three").innerHTML = UserAge;
 console.log(UserAge)
 
 let UserBrand = prompt('What is your favourite brand ?')
+document.getElementById("four").innerHTML = UserBrand;
 console.log(UserBrand)
 
 
 alert (' Hello '+ UserName +' from '+ UserCountry +' with '+UserAge +' years old '+'find your ' +UserBrand+' latest models ')
+
